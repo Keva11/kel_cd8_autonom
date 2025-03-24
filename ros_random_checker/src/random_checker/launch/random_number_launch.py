99fc19e2d081a5e6825875b9142cb0a2e85f04e3
Keva11 <kevin.kelemen11@gmail.com>
2025-03-24 12:12:42 +0100

    Update random_number_launch.py

--- ros_random_checker/src/random_checker/launch/random_number_launch.py
+++ ros_random_checker/src/random_checker/launch/random_number_launch.py
@@ -1,17 +1,18 @@
-
-from random_number_launch import LaunchDescription
-from random_number_launch_ros.actions import Node
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='random_checker',
             executable='random_number_publisher',
-            name='random_number_publisher'
+            name='random_number_publisher',
+            output='screen'
         ),
         Node(
             package='random_checker',
             executable='number_checker_subscriber',
-            name='number_checker_subscriber'
+            name='number_checker_subscriber',
+            output='screen'
         )
     ])
